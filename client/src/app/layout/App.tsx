a4c49bfc1ced8f6cc12cb18a38fec7536f7ea23c
Boolean-1024 <daishiwei.1024@gmail.com>
2024-12-15 10:01:33 +0800

    Completes the Product Details Page

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,31 +2,32 @@ import Catalog from "../../features/catalog/Catalog";
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' :'light'
+  const paletteType = darkMode ? 'dark' : 'light'
 
   const theme = createTheme({
-    palette:{
-      mode:paletteType,
-      background:{
-        default:paletteType ==='light' ? '#eaeaea' : '#121212'
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
       }
     }
   })
 
   return (
     <>
-    <ThemeProvider theme={theme}>      
-      <CssBaseline/>
-      <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
-      <Container>
-      <Catalog/>
-      </Container>
-     </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Container>
+          <Outlet />
+        </Container>
+      </ThemeProvider>
 
-      
+
     </>
   )
 }
